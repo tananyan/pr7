37619ec086238faf001c32e9877a18ddbba352ca
Boris <boris242008@mail.ru>
2022-02-05 20:31:31 +0300

    22 commit (animationsробит)

--- src/js/script.js
+++ src/js/script.js
@@ -143,4 +143,20 @@ $(document).ready(function () {
     });
     return false;
   });
+
+  // scrolling
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1500) {
+      $(".scroll-up").fadeIn("fast");
+      $(".scroll-down").fadeOut("fast");
+    } else {
+      $(".scroll-up").fadeOut("fast");
+      $(".scroll-down").fadeIn("fast");
+    }
+  });
+  // $("a[href^='#']").click(function () {
+  //   const _href = $(this).attr("href");
+  //   $("html, body").animate({ scrollTop: $(_href).offset().top + "+px" });
+  //   return false;
+  // });
 });
